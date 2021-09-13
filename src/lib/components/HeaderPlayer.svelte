d40d84c2033619157a70436a141541d324342cd6
Drew Dunn-Rankin <drewdunnrankin@gmail.com>
2021-09-13 14:18:11 -0700

    tailwind and some basics

---
+++ src/lib/components/HeaderPlayer.svelte
@@ -0,0 +1,9 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative bg-gray-800">
+	<div class="max-w-7xl mx-auto py-8 px-3 sm:px-6 lg:px-8">
+		<div class="text-white">
+			<a href="/" class="text-blue-100 px-4">Home</a>
+			<span>Player goes here</span>
+		</div>
+	</div>
+</div>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	import '../app.postcss';
+	import HeaderPlayer from '$lib/components/HeaderPlayer.svelte';
+</script>
+
+<HeaderPlayer />
+<div class="flex float-right border-l border-gray-200bg-white overflow-y-auto">
+	<div class="flex items-center">
+		<div class="border-r h-screen">
+			<a href="/tv" class="text-gray-600  hover:text-gray-900 items-center px-8 text-sm font-mono">
+				TV
+			</a>
+		</div>
+		<div class="border-r h-screen">
+			<a
+				href="/archive"
+				class="text-gray-600  hover:text-gray-900 items-center px-4 py-4 text-sm font-mono"
+			>
+				ARCHIVE
+			</a>
+		</div>
+		<div class="border-r h-screen">
+			<a
+				href="/about"
+				class="text-gray-600  hover:text-gray-900 items-center px-4 py-4 text-sm font-mono"
+			>
+				ABOUT
+			</a>
+		</div>
+	</div>
+</div>
+
+<slot />
